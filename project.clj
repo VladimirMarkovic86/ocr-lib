307435e17de55e23b252fc3007ad7f37a38c1255
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:46:29 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject org.vladimir/ocr-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.1.0"
   :description "Optical character recognition library"
-  :url "http://gitlab:1610/VladimirMarkovic86/ocr-lib"
+  :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/utils-lib "0.1.0"]]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/clj"])
+
