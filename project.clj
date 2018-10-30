290e55474fd48c142326731231cd928740aebc79
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-30 11:54:10 +0100

    Changing parameter for number of unknown signs to be listed, also upgrades version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.0"
   :description "Optical character recognition library"
   :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
