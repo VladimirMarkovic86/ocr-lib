6a9494f860e77ca7cf5233d7fa3683b5975b67d5
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-20 14:00:32 +0100

    Utils library project version was upgraded so its dependent projects change their versions also.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.1"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.2"
   :description "Optical character recognition library"
   :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.1"]
                  ]
 
   :min-lein-version "2.0.0"
