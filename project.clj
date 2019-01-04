2adb7fdf42c62456e7070778ba12386d0bdd976b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-04 12:40:41 +0100

    Task: 7 Design improved for ocr, po and sample projects.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.2"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.3"
   :description "Optical character recognition library"
   :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.1"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
                  ]
 
   :min-lein-version "2.0.0"
