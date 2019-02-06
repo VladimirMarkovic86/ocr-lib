925702c33c77a80c392d5cc4b1f2bd4ba462dd62
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-06 14:20:23 +0100

    Task 38: Implemented calories calculator

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.3"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.4"
   :description "Optical character recognition library"
   :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.3"]
                  ]
 
   :min-lein-version "2.0.0"
