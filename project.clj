06e79b6ad01da579f4aae5f5e9e03f43f08d7c7c
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-17 18:00:12 +0200

    Task 63: Bug fixing.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.9"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.10"
   :description "Optical character recognition library"
   :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
