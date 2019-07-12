c973fc5e8a87b7adaa293b767d05105ade998b49
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-12 15:50:42 +0200

    Task 88: Implemented droid-server project, it is as library to be used with AndroidServer android java project.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.13"
+(defproject org.clojars.vladimirmarkovic86/ocr-lib "0.2.14"
   :description "Optical character recognition library"
   :url "http://github.com/VladimirMarkovic86/ocr-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
                  ]
 
   :min-lein-version "2.0.0"
